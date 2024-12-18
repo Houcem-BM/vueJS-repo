de4cb9e2c7cb451834f86aa004f77fe29eb54b65
Houssem <houyahiya@gmail.com>
2024-12-18 20:13:52 +0100

    multi-word components

--- src/pages/appUsers.vue
+++ src/pages/appUsers.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
       <h1>Our Users</h1>
-      <UserCard name="John Doe" email="john.doe@example.com" />
-      <UserCard name="Jane Smith" email="jane.smith@example.com" />
+      <appUserCard name="John Doe" email="john.doe@example.com" />
+      <appUserCard name="Jane Smith" email="jane.smith@example.com" />
     </div>
   </template>
   
   <script>
-  import UserCard from '../components/appUserCard.vue';
+  import appUserCard from '../components/appUserCard.vue';
   
   export default {
     components: {
-      UserCard,
+      appUserCard,
     },
   };
   </script>
