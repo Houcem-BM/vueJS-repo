f0dabdeb6afc0b90927a9e39f0fba7c8b030f34c
Houssem <houyahiya@gmail.com>
2024-12-18 19:57:47 +0100

    SPA App

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './pages/Home.vue';
-import About from './pages/About.vue';
-import Users from './pages/Users.vue';
+import Home from './pages/Home-page.vue';
+import About from './pages/About-page.vue';
+import Users from './pages/Users-page.vue';
 
 const routes = [
   { path: '/', component: Home },
